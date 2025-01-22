0c936fdae36ac8d937d3ef641fd72a5d65bcdee3
Nicolò Curioni <nicolo.curioni96@gmail.com>
2025-01-22 15:39:32 +0100

    Initial Commit

---
+++ WritingAI Demo/WritingAI_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  WritingAI_DemoApp.swift
+//  WritingAI Demo
+//
+//  Created by Nicolò Curioni on 22/01/25.
+//
+
+import SwiftUI
+
+@main
+struct WritingAI_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
